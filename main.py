898bb0386d64d49350525ee57b2c84ded078836d
Piotr Derewonko <p.derewonko@fsaps.pl>
2022-09-06 12:13:24 +0200

    dodano opcje odswiezania danych do raportu glownego z poziomu cyklicznego skryptu. Dane do dash zaciagane sa z tabeli statycznej

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ st.set_page_config(page_title="Moduł raportowania dla firmy FSAPS",
 try:
     sorce_main = sys.argv[1]
 except:
-    sorce_main = 'local'
-mailings, con = deaful_set(f'{sorce_main}')
+    sorce_main = 'lwowska'
+mailings, con, engine = deaful_set(f'{sorce_main}')
 try:
     refresh_data = sys.argv[2]
 except:
@@ -33,15 +33,16 @@ with st.sidebar:
 year_from = year_range_slider[0]
 year_to = year_range_slider[1]
 
-data = download_dash_address_data(con, refresh_data)
+data = download_dash_address_data(con, refresh_data, engine)
 data_to_show = data.loc[(data['grupa_akcji_3'] >=year_from) & (data['grupa_akcji_3'] <=year_to)]
 
 # tworze pierwsza 3 zakladki dal mailnigu adresowego
 tab1, tab2, tab3 = st.tabs(['Wykres', 'Tabela przestawna', 'Sortowanie/Zmiany wykresu'])
 # todo dorobic tutaj mozliwosci zmiany sortowania oraz kolejnsoci grupowania
 with tab3:
-    st.button('test')
-    cam_adr_plot, test_pivot = pivot_and_chart_for_dash(data_to_show)
+    levels = st.multiselect(options=['grupa_akcji_3', 'grupa_akcji_2'], label='Proszę wybrać kolejność',
+                            default=['grupa_akcji_3', 'grupa_akcji_2'])
+    cam_adr_plot, test_pivot = pivot_and_chart_for_dash(data_to_show, levels)
 with tab1:
     st.bokeh_chart(cam_adr_plot)
 with tab2:
