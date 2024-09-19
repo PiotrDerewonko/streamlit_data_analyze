be848b70606ef1031026d44c31e894ada61f2269
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-09-19 11:44:24 +0200

    dodano opcje filtorwania

--- pages/10_chart_in_days.py
+++ pages/10_chart_in_days.py
@@ -21,74 +21,81 @@ with st.container():
     option_list = distinct_columns()
     choosed_options = st.multiselect(options=option_list, default=['grupa_akcji_2_wysylki', 'grupa_akcji_3_wysylki'],
                                      label='Wybierz dane do wykresu')
+    przelicz_dane = st.button('Wygenereuj dane')
 
     # wybor opcji do wykresow
     ChartsInDaysBasic = ChartsInDays(mailings, con, years, False, engine)
-    days_range, cumulative, new_old, chose_new_old, tab1, tab2, tab3, tab4, tab5 = ChartsInDaysBasic.create_tabs()
-    ChartsInDaysBasic.download_data()
+    days_range, cumulative, new_old, chose_new_old, tab1, tab2, tab3, tab4, tab5, dict_user_choice = ChartsInDaysBasic.create_tabs()
 
-    # tworze staly element tytulu
-    title_basic = change_list_to_string(qamp, 'dla mailingów') + change_list_to_string(years, ' za lata')
+    if przelicz_dane:
+        ChartsInDaysBasic.download_data()
+        ChartsInDaysBasic.filtr_data_by_user_choise(dict_user_choice)
 
-    # tworzenie tabeli przestawnej dla kosztu
-    CostAmount = CreatePivotTableAndChart(None, qamp, years, 1, 1,
-                                          ChartsInDaysBasic.data_cost_and_circulation, False, new_old, chose_new_old, 'koszt',
-                                          choosed_options)
-    pivot_table_cost_amount, list_of_columns_cost_amount = CostAmount.create_pivot_table()
+        # tworze staly element tytulu
+        title_basic = change_list_to_string(qamp, 'dla mailingów') + change_list_to_string(years, ' za lata')
 
-    # tworznie tabeli przestwnej dla nakladu
-    CircAmount = CreatePivotTableAndChart(None, qamp, years, 1, 1,
-                                          ChartsInDaysBasic.data_cost_and_circulation, False, new_old, chose_new_old, 'naklad',
-                                          choosed_options)
-    pivot_table_circ_amount, list_of_columns_circ_amount = CircAmount.create_pivot_table()
+        # tworzenie tabeli przestawnej dla kosztu
+        CostAmount = CreatePivotTableAndChart(None, qamp, years, 1, 1,
+                                              ChartsInDaysBasic.data_cost_and_circulation, False, new_old,
+                                              chose_new_old, 'koszt',
+                                              choosed_options)
+        pivot_table_cost_amount, list_of_columns_cost_amount = CostAmount.create_pivot_table()
 
-    # tworzenie tabeli przestwnej i wykresu dla sumy wplat
-    SumAmount = CreatePivotTableAndChart(tab1, qamp, years, days_range[0], days_range[-1],
-                                         ChartsInDaysBasic.data_sum_count,
-                                         cumulative, new_old, chose_new_old, 'suma_wplat', choosed_options)
-    pivot_table_sum_amount, list_of_columns_sum_amount = SumAmount.create_pivot_table()
-    pivot_table_sum_amount = SumAmount.change_pivot_table(pivot_table_sum_amount, list_of_columns_sum_amount)
-    pivot_table_sum_amount = SumAmount.customize_pivot_table_(pivot_table_sum_amount)
-    title_sum_amount = ' Wykres sumy wpłat ' + title_basic
-    sum_char = SumAmount.create_char_helper_custom(pivot_table_sum_amount, 'Suma wpłat', title_sum_amount,
-                                                   pivot_table_circ_amount)
-    SumAmount.put_data_into_streamlit(pivot_table_sum_amount, sum_char)
+        # tworznie tabeli przestwnej dla nakladu
+        CircAmount = CreatePivotTableAndChart(None, qamp, years, 1, 1,
+                                              ChartsInDaysBasic.data_cost_and_circulation, False, new_old,
+                                              chose_new_old, 'naklad',
+                                              choosed_options)
+        pivot_table_circ_amount, list_of_columns_circ_amount = CircAmount.create_pivot_table()
 
-    # tworzenie tabeli przestwnej i wykresu dla liczby wplat
-    CountAmount = CreatePivotTableAndChart(tab2, qamp, years, days_range[0], days_range[-1],
-                                           ChartsInDaysBasic.data_sum_count,
-                                           cumulative, new_old, chose_new_old, 'liczba_wplat', choosed_options)
-    pivot_table_count_amount, list_of_columns_count_amount = CountAmount.create_pivot_table()
-    pivot_table_count_amount = CountAmount.change_pivot_table(pivot_table_count_amount, list_of_columns_count_amount)
-    pivot_table_count_amount = CountAmount.customize_pivot_table_(pivot_table_count_amount)
-    title_count_amount = ' Wykres liczby wpłat ' + title_basic
-    count_char = CountAmount.create_char_helper_custom(pivot_table_count_amount, 'Liczba wpłat',
-                                                       title_count_amount, pivot_table_circ_amount)
-    CountAmount.put_data_into_streamlit(pivot_table_count_amount, count_char)
+        # tworzenie tabeli przestwnej i wykresu dla sumy wplat
+        SumAmount = CreatePivotTableAndChart(tab1, qamp, years, days_range[0], days_range[-1],
+                                             ChartsInDaysBasic.data_sum_count,
+                                             cumulative, new_old, chose_new_old, 'suma_wplat', choosed_options)
+        pivot_table_sum_amount, list_of_columns_sum_amount = SumAmount.create_pivot_table()
+        pivot_table_sum_amount = SumAmount.change_pivot_table(pivot_table_sum_amount, list_of_columns_sum_amount)
+        pivot_table_sum_amount = SumAmount.customize_pivot_table_(pivot_table_sum_amount)
+        title_sum_amount = ' Wykres sumy wpłat ' + title_basic
+        sum_char = SumAmount.create_char_helper_custom(pivot_table_sum_amount, 'Suma wpłat', title_sum_amount,
+                                                       pivot_table_circ_amount)
+        SumAmount.put_data_into_streamlit(pivot_table_sum_amount, sum_char)
 
-    # tworzenie tabeli przestawnej dla SZLW
-    SZLW = CreatePivotTableAndChart(tab3, qamp, years, None, None, None, None,
-                                    None, None, None, choosed_options)
-    szlw_pivot = SZLW.calculation_roi_or_szlw(pivot_table_count_amount, pivot_table_circ_amount, 'div')
-    title_szlw_amount = ' Wykres stopy zwrotu liczby wpłat ' + title_basic
-    szlw_char = SZLW.create_char_helper_custom_wo_pivot_class(szlw_pivot, 'Stopa zwrotu liczby wpłat',
-                                                              title_szlw_amount, pivot_table_circ_amount)
-    SZLW.put_data_into_streamlit(szlw_pivot, szlw_char)
+        # tworzenie tabeli przestwnej i wykresu dla liczby wplat
+        CountAmount = CreatePivotTableAndChart(tab2, qamp, years, days_range[0], days_range[-1],
+                                               ChartsInDaysBasic.data_sum_count,
+                                               cumulative, new_old, chose_new_old, 'liczba_wplat', choosed_options)
+        pivot_table_count_amount, list_of_columns_count_amount = CountAmount.create_pivot_table()
+        pivot_table_count_amount = CountAmount.change_pivot_table(pivot_table_count_amount,
+                                                                  list_of_columns_count_amount)
+        pivot_table_count_amount = CountAmount.customize_pivot_table_(pivot_table_count_amount)
+        title_count_amount = ' Wykres liczby wpłat ' + title_basic
+        count_char = CountAmount.create_char_helper_custom(pivot_table_count_amount, 'Liczba wpłat',
+                                                           title_count_amount, pivot_table_circ_amount)
+        CountAmount.put_data_into_streamlit(pivot_table_count_amount, count_char)
 
-    # tworzenie tabeli przestawnej dla ROI
-    ROI = CreatePivotTableAndChart(tab4, qamp, years, None, None, None, None,
-                                   None, None, None, choosed_options)
-    roi_pivot = ROI.calculation_roi_or_szlw(pivot_table_sum_amount, pivot_table_cost_amount, 'div')
-    title_roi_amount = ' Wykres ROI ' + title_basic
-    roi_char = ROI.create_char_helper_custom_wo_pivot_class(roi_pivot, 'ROI',
-                                                            title_roi_amount, pivot_table_circ_amount)
-    ROI.put_data_into_streamlit(roi_pivot, roi_char)
+        # tworzenie tabeli przestawnej dla SZLW
+        SZLW = CreatePivotTableAndChart(tab3, qamp, years, None, None, None, None,
+                                        None, None, None, choosed_options)
+        szlw_pivot = SZLW.calculation_roi_or_szlw(pivot_table_count_amount, pivot_table_circ_amount, 'div')
+        title_szlw_amount = ' Wykres stopy zwrotu liczby wpłat ' + title_basic
+        szlw_char = SZLW.create_char_helper_custom_wo_pivot_class(szlw_pivot, 'Stopa zwrotu liczby wpłat',
+                                                                  title_szlw_amount, pivot_table_circ_amount)
+        SZLW.put_data_into_streamlit(szlw_pivot, szlw_char)
 
-    # tworzenie tabeli przestawnej dla profitu
-    profit = CreatePivotTableAndChart(tab5, qamp, years, None, None, None, None,
-                                      None, None, None, choosed_options)
-    profit_pivot = profit.calculation_roi_or_szlw(pivot_table_sum_amount, pivot_table_cost_amount, 'subtract')
-    title_profit_amount = ' Wykres profitu ' + title_basic
-    profit_char = profit.create_char_helper_custom_wo_pivot_class(profit_pivot, 'Profit',
-                                                                  title_profit_amount, pivot_table_circ_amount)
-    profit.put_data_into_streamlit(profit_pivot, profit_char)
+        # tworzenie tabeli przestawnej dla ROI
+        ROI = CreatePivotTableAndChart(tab4, qamp, years, None, None, None, None,
+                                       None, None, None, choosed_options)
+        roi_pivot = ROI.calculation_roi_or_szlw(pivot_table_sum_amount, pivot_table_cost_amount, 'div')
+        title_roi_amount = ' Wykres ROI ' + title_basic
+        roi_char = ROI.create_char_helper_custom_wo_pivot_class(roi_pivot, 'ROI',
+                                                                title_roi_amount, pivot_table_circ_amount)
+        ROI.put_data_into_streamlit(roi_pivot, roi_char)
+
+        # tworzenie tabeli przestawnej dla profitu
+        profit = CreatePivotTableAndChart(tab5, qamp, years, None, None, None, None,
+                                          None, None, None, choosed_options)
+        profit_pivot = profit.calculation_roi_or_szlw(pivot_table_sum_amount, pivot_table_cost_amount, 'subtract')
+        title_profit_amount = ' Wykres profitu ' + title_basic
+        profit_char = profit.create_char_helper_custom_wo_pivot_class(profit_pivot, 'Profit',
+                                                                      title_profit_amount, pivot_table_circ_amount)
+        profit.put_data_into_streamlit(profit_pivot, profit_char)
--- pages/ma_details_files/charts_in_days/charts_in_days_basic.py
+++ pages/ma_details_files/charts_in_days/charts_in_days_basic.py
@@ -1,9 +1,10 @@
-from typing import Tuple, Any
+from typing import Tuple, Any, Dict, List
 
 import pandas as pd
 import streamlit as st
 
 from pages.ma_details_files.charts_in_days.download_data_for_days_charts import download_data_for_days_charts
+from pages.ma_details_files.data_about_people_and_campaign_pay import distinct_options
 from pages.ma_details_files.data_about_people_and_campaign_pay import download_data_about_people
 
 
@@ -17,7 +18,27 @@ class ChartsInDays:
         self.data_cost_and_circulation = None
         self.data_sum_count = None
 
-    def create_tabs(self) -> Tuple[Any, Any, Any, Any, Any, Any, Any, Any, Any]:
+    @staticmethod
+    def choose_filter_options() -> Dict[Any, List[Any]]:
+        """Metoda dodaje opcje filtrowania danych po kolumnach. Zwraca słownik, gdzie kluczem jest nazwa kolumny,
+        a wartoscia jest lista z wybranymi opcjami."""
+        filter_value = distinct_options(False)
+        list_of_filter_value = filter_value.columns.to_list()
+
+        # zamieniam pierwszy element z id_korespondenta na pusta wartosc
+        list_of_filter_value[0] = ' '
+
+        filtr_1_column_name = st.selectbox(label="Pierwszy filtr", options=list_of_filter_value)
+        filtr_1_values = st.multiselect(label="Wybierz wartosci",
+                                        options=filter_value[filtr_1_column_name].dropna().drop_duplicates())
+
+        st.markdown(filtr_1_column_name, unsafe_allow_html=True)
+        st.markdown(filtr_1_values, unsafe_allow_html=True)
+
+        dict_to_return = {filtr_1_column_name: filtr_1_values}
+        return dict_to_return
+
+    def create_tabs(self) -> Tuple[Any, Any, Any, Any, Any, Any, Any, Any, Any, Dict[Any, List[Any]]]:
         """Metoda tworzy zakladki potrzebne, w których wybieramy jakie dane mają znaleść się na wykresie. 
         Dodatkowo metoda umiesza wygenerowane wykresy i tabele przestawne """
         with st.container():
@@ -33,8 +54,8 @@ class ChartsInDays:
                 chose_new_old = st.multiselect(label='Kogo wyświetlać', options=['nowy', 'stary'],
                                                default=['nowy', 'stary'])
             with tab7:
-                st.markdown("dodaj filtry")
-        return days_range, cumulative, new_old, chose_new_old, tab1, tab2, tab3, tab4, tab5
+                dict_of_user_choice = self.choose_filter_options()
+        return days_range, cumulative, new_old, chose_new_old, tab1, tab2, tab3, tab4, tab5, dict_of_user_choice
 
     def download_data(self):
         """Metoda pobiera dane niezbedne do dalszej analizy."""
@@ -46,8 +67,17 @@ class ChartsInDays:
         self.data_cost_and_circulation['koszt'] = self.data_cost_and_circulation['koszt'].astype(float)
         self.data_cost_and_circulation['grupa_akcji_3_wysylki'] = self.data_cost_and_circulation[
             'grupa_akcji_3_wysylki'].astype(str)
-        data_about_people = download_data_about_people(self.con, False, 1000, [])
+        data_about_people = download_data_about_people(self.con, False, 10000, [])
         self.data_sum_count = pd.merge(self.data_sum_count, data_about_people, on='id_korespondenta', how='left')
         self.data_sum_count['grupa_akcji_3_wysylki'] = self.data_sum_count['grupa_akcji_3_wysylki'].astype(str)
         self.data_cost_and_circulation = pd.merge(self.data_cost_and_circulation, data_about_people,
                                                   on='id_korespondenta', how='left')
+
+    def filtr_data_by_user_choise(self, filtr_value) -> None:
+        """Metoda filtruje dane na podsatwie wybranych przez uzuytkonika danych w zakaldce filtry"""
+        for i, j in filtr_value.items():
+            column_to_filter = i
+            values_to_filter = j
+            self.data_sum_count = self.data_sum_count.loc[self.data_sum_count[column_to_filter].isin(values_to_filter)]
+            self.data_cost_and_circulation = self.data_cost_and_circulation.loc[
+                self.data_cost_and_circulation[column_to_filter].isin(values_to_filter)]
